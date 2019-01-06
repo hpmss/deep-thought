still trying to fix the gradient

deepfwnet.py
@@ -5,6 +5,13 @@ import sys
 from scipy import sparse
 import tensorflow as tf
 
+
+def sigmoid(z):
+    return 1.0/(1.0+np.exp(-z))
+
+def sigmoid_prime(z):
+    return sigmoid(z)*(1-sigmoid(z))
+
 def convert_onehot(values):
     a = np.zeros((values.size,values.max() + 1))
     a[np.arange(values.size),values] = 1
@@ -44,62 +51,69 @@ class DeepFeedForwardNetwork(object):
         z = e_z / e_z.sum(axis=0)
         return z
 
-    def feedforward(self, a):
+    def feed_forward(self, a):
         for i in range(self.layers):
             z = np.dot(self.w_matrix[i].T, a)+self.b_matrix[i]
-            if i == self.layers - 1:
-                a = self.softmax(z)
-            else:
-                a = np.maximum(z,0)
+            # if i == self.layers - 1:
+            #     a = self.softmax(z)
+            # else:
+            #     a = np.maximum(z,0)
+            a = sigmoid(z)
         return a
 
     #Single input_output
-    def feed_forward(self,x_mini_batch,w_matrix,b_matrix):
-        all_output = []
-        for input in x_mini_batch.T:
-            input = input.reshape(input.shape[0],1)
-            z_vectors = []
-            a_vectors = [input]
-            a_current = input
-            for i in range(self.layers):
-                z_i = np.dot(w_matrix[i].T,a_current) + b_matrix[i]
-                z_vectors.append(z_i)
-                if i == self.layers - 1:
-                    a_i = self.softmax(z_i)
-                else:
-                    a_i = np.maximum(z_i,0)
-
-                a_vectors.append(a_i)
-                a_current = a_i
-            all_output.append((z_vectors,a_vectors))
-
-        return all_output
-
-    #From last output to first
-    def backprop(self,all_output,w_matrix,b_matrix,y):
+    def feed_forward_and_update(self,x_mini_batch,y_mini_batch,w_matrix,b_matrix):
         nabla_w = [np.zeros(w_matrix.shape) for w_matrix in w_matrix]
         nabla_b = [np.zeros(b_matrix.shape) for b_matrix in b_matrix]
-        for (z_vectors,a_vectors),y_label in zip(all_output,y.T):
-            y_label = y_label.reshape(y_label.shape[0],1)
-            e = [(a_vectors.pop() - y_label)] #e_last
-            delta_nabla_b = []
-            delta_nabla_w = []
-            e_next = 0
-            for i in range(0,self.layers):
-                a_vector = a_vectors[-(i+1)]
-                z_vector = z_vectors[-(i+1)]
-                if i + 1 != 1:
-                    e[-1][z_vector <= 0] = 0 #reLU derivative
-                nabla_w_i = np.dot(a_vector,e[-1].T)
-                nabla_b_i = e[-1]
-
-                delta_nabla_w.insert(0,nabla_w_i)
-                delta_nabla_b.insert(0,nabla_b_i)
-                e_next = np.dot(w_matrix[-(i+1)],e[-1])
-                e.append(e_next)
+        for input,output in zip(x_mini_batch.T,y_mini_batch.T):
+            input = input.reshape(input.shape[0],1)
+            output = output.reshape(output.shape[0],1)
+            delta_nabla_w,delta_nabla_b = self.backprop(input,output,self.w_matrix,self.b_matrix)
             nabla_w = [nab_w + del_nab_w for nab_w,del_nab_w in zip(nabla_w,delta_nabla_w)]
             nabla_b = [nab_b + del_nab_b for nab_b,del_nab_b in zip(nabla_b,delta_nabla_b)]
+            print(nabla_w)
         return nabla_w,nabla_b
+
+    #From last output to first
+    def backprop(self,x,y,w_matrix,b_matrix):
+        a_vectors = [x]
+        a_current = x
+        z_vectors = []
+
+        #feed-forward
+        for i in range(self.layers):
+            z_i = np.dot(w_matrix[i].T,a_current) + b_matrix[i]
+            z_vectors.append(z_i)
+
+            # if i == self.layers - 1:
+            #     a_i = self.softmax(z_i)
+            # else:
+            #     a_i = np.maximum(z_i,0)
+            a_i = sigmoid(z_i)
+
+            a_vectors.append(a_i)
+            a_current = a_i
+
+        #back-propagation
+        e = [(a_vectors.pop() - y)] #e_last
+        delta_nabla_b = []
+        delta_nabla_w = []
+        e_next = 0
+        for i in range(0,self.layers):
+            a_vector = a_vectors[-(i+1)]
+            z_vector = z_vectors[-(i+1)]
+            # if i + 1 != 1:
+            #     e[-1][z_vector <= 0] = 0 #reLU derivative
+            # else:
+            e[-1] = e[-1] * sigmoid_prime(z_vector)
+            nabla_w_i = np.dot(a_vector,e[-1].T)
+            nabla_b_i = e[-1]
+
+            delta_nabla_w.insert(0,nabla_w_i)
+            delta_nabla_b.insert(0,nabla_b_i)
+            e_next = np.dot(w_matrix[-(i+1)],e[-1])
+            e.append(e_next)
+        return delta_nabla_w,delta_nabla_b
 
     def save_network(self,filename,location='#currentWorkDir#'):
         unallowed_characters = "/\\?%*:|\"<>"
@@ -137,23 +151,32 @@ class DeepFeedForwardNetwork(object):
             sys.exit(1)
 
 
-    def train(self,epoch=15):
+    def train(self,epoch=15,test_data = None):
         x_mini_batches = [self.x[:,k:k+self.mini_batch_size] for k in range(0,self.x.shape[1],self.mini_batch_size)]
         y_mini_batches = [self.y[:,k:k+self.mini_batch_size] for k in range(0,self.y.shape[1],self.mini_batch_size)]
         #feed-forward
         for epoc in range(epoch):
-            print("Epoch: ",epoc + 1)
+            print("-> epoch: ",epoc + 1)
             for x_mini_batch,y_mini_batch in zip(x_mini_batches,y_mini_batches):
-                for i in range(self.iteration):
-                    all_output = self.feed_forward(x_mini_batch,self.w_matrix,self.b_matrix)
+                nabla_w,nabla_b = self.feed_forward_and_update(x_mini_batch,y_mini_batch,self.w_matrix,self.b_matrix)
 
-                    nabla_w , nabla_b = self.backprop(all_output,self.w_matrix,self.b_matrix,y_mini_batch)
-                    self.w_matrix = [w_matrix - (self.eta/self.mini_batch_size)*nabla_w_matrix for w_matrix,nabla_w_matrix in zip(self.w_matrix,nabla_w)]
-                    self.b_matrix = [b_matrix - (self.eta/self.mini_batch_size)*nabla_b_matrix for b_matrix,nabla_b_matrix in zip(self.b_matrix,nabla_b)]
-                # a_vector = self.feedforward(x_mini_batch)
-                # loss = self.cost(a_vector,y_mini_batch)
-                # print('epoc %d,loss: %f' %(epoc + 1,loss))
+                self.w_matrix = [w_matrix - (self.eta/len(x_mini_batch))*nabla_w_matrix for w_matrix,nabla_w_matrix in zip(self.w_matrix,nabla_w)]
+                self.b_matrix = [b_matrix - (self.eta/len(x_mini_batch))*nabla_b_matrix for b_matrix,nabla_b_matrix in zip(self.b_matrix,nabla_b)]
+            if test_data:
+                print("-> epoch {} result: {} / {}".format(epoc + 1,self.evaluate(test_data),len(test_data[1])))
+            else:
+                print("-> epoch {} completed...")
 
+    def evaluate(self,test_data):
+        x_test , y_test = test_data
+        test_results = []
+        for x,y in zip(x_test.T,y_test):
+            x = x.reshape(x.shape[0],1)
+            y = y.reshape(y.shape[0],1)
+            a = np.argmax(self.feed_forward(x))
+            b = y
+            test_results.append((a,b))
+        return sum(int(x == y) for (x, y) in test_results)
 
 if __name__ == '__main__':
     (x_train,y_train) , (x_test,y_test) = tf.keras.datasets.cifar10.load_data()
@@ -161,9 +184,9 @@ if __name__ == '__main__':
     x_train = x_train.reshape(x_train.shape[0],32*32*3).T
     y_train = y_train.T.reshape(y_train.shape[0])
     y_train = convert_onehot(y_train)
+
+    x_test = x_test.reshape(x_test.shape[0],32*32*3).T
     sizes = [3072,100,10]
-    w_matrix = [np.ones((x,y)) for x,y in zip(sizes[:-1],sizes[1:])]
-    b_matrix = [np.ones((y,1)) for y in sizes[1:]]
     network = DeepFeedForwardNetwork(x_train,y_train,sizes,eta=0.9,iteration=1)
-    network.train(epoch=30)
+    network.train(epoch=15,test_data = (x_test , y_test))
     network.save_network('network_save.dat')